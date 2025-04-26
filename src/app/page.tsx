bb91cd445d3bfba5b6b300cd1d9cda1fcb3dbdfe
s1avo <s.nadolny@hotmail.com>
2025-04-26 15:50:24 +0200

    ref: adjust h1 heading size for mobile screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { ModalLink } from '@/ui/modal-link';
 export default function Home() {
 	return (
 		<div className="relative min-h-svh">
-			<header className="bg-muted flex h-24 items-center justify-between px-16">
-				<h1 className="text-3xl">
+			<header className="bg-muted flex h-24 items-center justify-between px-4 sm:px-16">
+				<h1 className="text-xl sm:text-3xl">
 					<span className="font-bold">Next.js</span> | Modal Form
 				</h1>
 				<ModalLink />
